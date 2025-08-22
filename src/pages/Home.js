59eb3f6ce767efa631f61a0eb9776af41f47f395
Tumekie Blackwood <omarblackwood25@gmail.com>
2025-08-21 22:07:28 -0500

    added signing component and styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import BoookingSignIn from '../components/BookingSignIn';
 
 function Home() {
-  return <h1>Welcome to Booking App</h1>;
+  return (
+    <div style={{ padding: '20px' , display: 'flex', flexDirection: 'column', alignItems: 'center',  }}>
+      <h1>Welcome to Tumekie's Booking App</h1>
+      <div style={{ maxWidth: 600, textAlign: 'center', marginBottom: 20 }}>
+        <p>This application allows you to book time slots for various services. You can view available time slots, make bookings, and manage your appointments.</p>
+        <p>To get started, please sign in or create an account.</p>
+      </div>
+      <div style={{ marginTop: -200}}>
+        <BoookingSignIn/>
+      </div>
+    </div>
+  )
 }
 
 export default Home;
